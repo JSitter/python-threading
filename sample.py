9baec3721d42b5e8dabdc19e397b872abd446265
Justin Sitter <justin@jaytria.com>
2018-02-05 14:33:10 -0800

    updated sample pomodoro timer

--- sample.py
+++ sample.py
@@ -4,21 +4,23 @@
 from threading import Thread
 import time
 
-def timer(name, delay, repeat):
-    print("timer: " + name + " started")
-    while repeat > 0:
-        time.sleep(delay)
-        print(name + ": " + str(time.ctime(time.time())))
-        repeat -= 1
-    print("Timer: " + name + " completed")
+def pomodoro(name, set_time):
+    print("Pomodoro : " + name + " start")
+    cur_time = 0
+    while cur_time < set_time:
+        time.sleep(1)
+        print(name + " timer" )
+        cur_time += 1
+
+    print( 'POMODORO! ' + name)
 
 def Main():
-    t1 = Thread(target=timer, args=("Timer1", 1, 5))
-    t2 = Thread(target=timer, args=("Timer2", 2, 5))
-    t1.start()
-    t2.start()
+    p1 = Thread(target=pomodoro, args=("Go Outside", 8))
+    p2 = Thread(target=pomodoro, args=("Get Coffee", 4))
+    p1.start()
+    p2.start()
 
-    print("Main completed")
+    print("Main Completed!")
 
 if __name__ == "__main__":
     Main()
