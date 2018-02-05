8ca08b8227e9aa9ee5320caebe7f587a380d781b
Justin Sitter <justin@jaytria.com>
2018-02-05 14:37:03 -0800

    simplified python example

--- sample.py
+++ sample.py
@@ -9,18 +9,16 @@ def pomodoro(name, set_time):
     cur_time = 0
     while cur_time < set_time:
         time.sleep(1)
-        print(name + " timer" )
+        print(name + " timer running" )
         cur_time += 1
 
     print( 'POMODORO! ' + name)
 
-def Main():
-    p1 = Thread(target=pomodoro, args=("Go Outside", 8))
-    p2 = Thread(target=pomodoro, args=("Get Coffee", 4))
-    p1.start()
-    p2.start()
 
-    print("Main Completed!")
+p1 = Thread(target=pomodoro, args=("Go Outside", 8))
+p2 = Thread(target=pomodoro, args=("Get Coffee", 4))
+p1.start()
+p2.start()
 
-if __name__ == "__main__":
-    Main()
+print("Main Completed!")
+
